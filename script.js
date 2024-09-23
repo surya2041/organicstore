66e342574f984dec0bd6107e6edf85c9d9ba9c22
surya2041 <tamangsurya2005@gmail.com>
2024-09-23 19:39:05 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// document.getElementById("price").innerHTML = "$0.00";
+
+// function shownavbar(){
+//     document.querySelector("nav div ul").style.display = "block";
+
+// }
+// function shownavbar(){
+//     document.querySelector("nav div ul").style.display = "block";
+// }
+
+function shownavbar(){
+    var list = document.querySelector(".mobile");
+    list.classList.toggle("list");
+    list.classList.toggle("showdesign");
+}
